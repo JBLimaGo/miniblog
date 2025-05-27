ab2c49deb231059ad079e0ec5e27f34d809d9bd1
Jefferson Lima <jbldesenvolvimento@gmail.com>
2025-05-27 15:20:49 -0300

    Função de Logout, Estruturando página de login, Bloqueando páginas que precisam de autenticação

--- src/App.js
+++ src/App.js
@@ -18,9 +18,10 @@ import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Login from "./pages/Login/Login";
 import Register from "./pages/Register/Register";
+import CreatePost from "./pages/CreatePost/CreatePost";
+import Dashboard from "./pages/Dashboard/Dashboard";
 
 function App() {
-
   const [user, setUser] = useState(undefined);
   const { auth } = useAuthentication();
 
@@ -31,12 +32,10 @@ function App() {
       setUser(user);
       if (user) {
         console.log("Usuário autenticado:", user);
-      } 
-      else {
+      } else {
         console.log("Usuário não autenticado");
       }
-    }
-    );
+    });
     return () => unsubscribe();
   }, [auth]);
 
@@ -53,8 +52,23 @@ function App() {
             <Routes>
               <Route path="/" element={<Home />} />
               <Route path="/about" element={<About />} />
-              <Route path="/login" element={<Login />} />
-              <Route path="/register" element={<Register />} />
+              <Route
+                path="/login"
+                element={!user ? <Login /> : <Navigate to="/" />}
+              />
+              <Route
+                path="/register"
+                element={!user ? <Register /> : <Navigate to="/" />}
+              />
+              <Route
+                path="/posts/create"
+                element={user ? <CreatePost /> : <Navigate to="/login" />}
+              />
+              <Route
+                path="/dashboard"
+                element={user ? <Dashboard /> : <Navigate to="/login" />}
+              />
+              {}
             </Routes>
           </div>
           <Footer />
--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,9 +1,75 @@
 import style from "./Login.module.css";
 
+import { useState, useEffect } from "react";
+
+import { useAuthentication } from "../../hooks/useAuthentication";
+
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const { createUser, error: authError, loading } = useAuthentication();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setError("");
+
+    const user = {
+      email,
+      password,
+    };
+
+    const res = await createUser(user);
+
+    // Simulate registration logic
+    console.log("Usuário registrado:", user);
+  };
+
+  useEffect(() => {
+    if (authError) {
+      setError(authError);
+    }
+  }, [authError]);
+
   return (
-    <div>
-      <h2>Login Page</h2>
+    <div className={style.login}>
+      <h1> Entrar</h1>
+      <p>Faça o login para poder utilizar o sistema</p>
+      <form onSubmit={handleSubmit}>
+        <p></p>
+        <label>
+          <span>E-mail:</span>
+          <input
+            type="email"
+            name="email"
+            required
+            placeholder="E-mail do usuário"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </label>
+        <p></p>
+        <label>
+          <span>Senha:</span>
+          <input
+            type="password"
+            name="password"
+            required
+            placeholder="Insira sua senha"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </label>
+        <p></p>
+        {!loading && <button className="btn">Entrar</button>}
+        {loading && (
+          <button className="btn" disabled>
+            Aguarde...
+          </button>
+        )}
+        {error && <p className="error">{error}</p>}
+      </form>
     </div>
   );
 };
