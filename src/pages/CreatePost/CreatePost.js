5472fa07fc281536c1ce75806129f06e021f5e9b
Jefferson Lima <jbldesenvolvimento@gmail.com>
2025-05-30 11:07:40 -0300

    Função Post e Inserindo dados no FireBase

--- src/pages/CreatePost/CreatePost.js
+++ src/pages/CreatePost/CreatePost.js
@@ -1,9 +1,109 @@
 import styles from "./CreatePost.module.css";
 
-const CreatePost = () => {
-  return (
-    <div>CreatePost</div>
-  )
-}
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useAuthValue } from "../../context/AuthContext";
+import { useInsertDocument } from "../../hooks/useInsertDocument";
 
-export default CreatePost
+const CreatePost = () => {
+  const [title, setTitle] = useState("");
+  const [image, setImage] = useState("");
+  const [body, setBody] = useState("");
+  const [tags, setTags] = useState("");
+  const [formError, setFormError] = useState("");
+
+  const { user } = useAuthValue();
+
+  const { insertDocument, response } = useInsertDocument("posts");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setFormError("");
+
+    // Validate form
+    if (!title || !image || !body) {
+      setFormError("Please fill in all fields");
+      return;
+    }
+
+     // Converter string de tags em array
+    const tagsArray = tags.split(",").map((tag) => tag.trim().toLowerCase());
+
+    // Create post object
+    insertDocument({
+      title,
+      image,
+      body,
+      tags: tagsArray,
+      uid: user.uid,
+      createdBy: user.displayName,
+    });
+
+    // Redirect user to home page after submission
+    // Submit post to API or context
+    // console.log("Post created:", post);
+  };
+
+  return (
+    <div className={styles.create_post}>
+      <h2>Criar Post</h2>
+      <p> Escreva sobre o que quiser e compartilhe o seu conhecimento! </p>
+      <form onSubmit={handleSubmit}>
+        <label>
+          <span>Título:</span>
+          <input
+            type="text"
+            name="title"
+            required
+            placeholder="Pense num bom título..."
+            onChange={(e) => setTitle(e.target.value)}
+            value={title}
+          />
+        </label>
+
+        <label>
+          <span>URL da imagem:</span>
+          <input
+            type="text"
+            name="image"
+            required
+            placeholder="Insira uma imagem que representa o post"
+            onChange={(e) => setImage(e.target.value)}
+            value={image}
+          />
+        </label>
+
+        <label>
+          <span>Conteúdo:</span>
+          <textarea
+            name="body"
+            required
+            placeholder="Insira o conteúdo do post"
+            onChange={(e) => setBody(e.target.value)}
+            value={body}
+          ></textarea>
+        </label>
+
+        <label>
+          <span>Tags:</span>
+          <input
+            type="text"
+            name="tags"
+            placeholder="Insira as tags separadas por vírgula"
+            onChange={(e) => setTags(e.target.value)}
+            value={tags}
+          />
+        </label>
+        {!response.loading && <button className="btn">Cadastrar</button>}
+        {response.loading && (
+          <button className="btn" disabled>
+            Aguarde...
+          </button>
+        )}
+        {response.error && <p className="error">{response.error}</p>}
+      </form>
+    </div>
+  );
+};
+
+export default CreatePost;
