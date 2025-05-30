f135d177980d74a977d41303b1399157449c9b81
Jefferson Lima <jbldesenvolvimento@gmail.com>
2025-05-30 13:45:29 -0300

    Estruturando o Home

--- src/pages/CreatePost/CreatePost.js
+++ src/pages/CreatePost/CreatePost.js
@@ -16,18 +16,27 @@ const CreatePost = () => {
 
   const { insertDocument, response } = useInsertDocument("posts");
 
+  const navigate = useNavigate();
+
   const handleSubmit = (e) => {
     e.preventDefault();
     setFormError("");
 
     // Validate form
-    if (!title || !image || !body) {
-      setFormError("Please fill in all fields");
-      return;
+    try {
+      new URL(image);
+    } catch (error) {
+      setFormError("A imagem precisa ser uma URL.");
     }
 
-     // Converter string de tags em array
+    // Converter string de tags em array
     const tagsArray = tags.split(",").map((tag) => tag.trim().toLowerCase());
+
+    if (title === "" || image === "" || body === "") {
+      setFormError("Por favor, preencha todos os campos.");
+    }
+
+    if (formError) return;
 
     // Create post object
     insertDocument({
@@ -40,6 +49,7 @@ const CreatePost = () => {
     });
 
     // Redirect user to home page after submission
+    navigate("/");
     // Submit post to API or context
     // console.log("Post created:", post);
   };
@@ -101,6 +111,7 @@ const CreatePost = () => {
           </button>
         )}
         {response.error && <p className="error">{response.error}</p>}
+        {formError && <p className="error">{formError}</p>}
       </form>
     </div>
   );
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,10 +1,42 @@
 //CSS
 import styles from "./Home.module.css";
 
+// hooks
+import { useNavigate, Link } from "react-router-dom";
+import { useState } from "react";
+
 const Home = () => {
+  const [query, setQuery] = useState([]);
+  const [posts] = useState([]);
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    navigate(`/search?q=${query}`);
+  };
+
   return (
-    <div>
-      <h1>Bem-vindo ao MiniBlog - Home</h1>
+    <div className={styles.home}>
+      <h1>Bem-vindo ao MiniBlog - posts mais recentes</h1>
+      <form onSubmit={handleSubmit} className={styles.search_form}>
+        <input
+          type="text"
+          placeholder="Pesquisar posts"
+          onChange={(e) => setQuery(e.target.value)}
+        />
+        <button className="btn btn-dark">Pesquisar</button>
+      </form>
+      <div>
+        <h1>Posts recentes</h1>
+        {posts && posts.length === 0 && (
+          <div className={styles.noposts}>
+            <p>Não há posts cadastrados</p>
+            <Link to="/posts/create" className="btn">
+              Criar primeiro post
+            </Link>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
